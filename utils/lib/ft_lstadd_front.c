741aa2201e5a6156a55fcc21288d92cdb4e40d72
Rambeau <104406218+ElmehdiBennix@users.noreply.github.com>
2023-03-29 01:46:49 +0000

    Update ft_lstadd_front.c

--- utils/lib/ft_lstadd_front.c
+++ utils/lib/ft_lstadd_front.c
@@ -12,8 +12,8 @@
 
 #include "../../push_swap.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *node)
 {
-	new -> next = *lst;
-	*lst = new;
+	node -> next = *lst;
+	*lst = node;
 }
